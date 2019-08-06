4c1ddc63346c7c5bf27e893e59770179ee10a7f3
MinoruSoga <minoru.2021@icloud.com>
2019-08-06 14:38:44 +0900

    branch b

--- first.java
+++ first.java
@@ -1,6 +1,6 @@
 public class present{
     public static void main(String[]args){
         System.out.println("HelloJava");
-        System.out.println("branch a");
+        System.out.println("branch b");
     }
 }
